61418f1dc2b146d7efb262db37df6524099e089d
Tianen Cheng <92861361+ob-cheng@users.noreply.github.com>
2025-06-21 15:50:11 +0200

    caesar_cipher.py

--- caesar_cipher_wip.py
+++ caesar_cipher_wip.py
@@ -1,27 +1,45 @@
+# import art
+print("""           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+""")
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-# direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+def caesar(original_text, shift_amount, encode_or_decode):
+    output_text = ""
 
+    if encode_or_decode == "decode":
+        shift_amount *= -1
 
-# TODO-1: Create a function called 'encrypt()' that takes 'original_text' and 'shift_amount' as 2 inputs.
-def encrypt(original_text, shift_amount):
-    # TODO-2: Inside the 'encrypt()' function, shift each letter of the 'original_text' forwards in the alphabet
-    #  by the shift amount and print the encrypted text.
-    original_index = []
-    encrypted_text = ""
     for letter in original_text:
-        original_index.append(alphabet.index(letter))
-    # print(original_index)
-    for each in original_index:
-        # print(alphabet[each + shift_amount])
-        # TODO-4: What happens if you try to shift z forwards by 9? Can you fix the code?
-        if each + shift_amount > 25:
-            encrypted_text += alphabet[(each + shift_amount)%25-1]
+        if letter in alphabet:
+            shifted_position = alphabet.index(letter) + shift_amount
+            shifted_position %= len(alphabet)
+            output_text += alphabet[shifted_position]
         else:
-            encrypted_text += alphabet[each + shift_amount]
-    print(encrypted_text)
-# TODO-3: Call the 'encrypt()' function and pass in the user inputs. You should be able to test the code and encrypt a
-#  message.
-encrypt(text, shift)
+            output_text += letter
+    print(f"Here is the {encode_or_decode}d result: {output_text}")
+
+is_continue = "yes"
+
+while is_continue == "yes":
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
+    is_continue = input("Type 'yes' if you want to go again. Otherwise, type 'no'.\n").lower()
